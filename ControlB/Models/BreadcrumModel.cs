feb11456d243b6ed1d853eb31c2787ce9367fc55
cromero <cromero@vertical.codaltec.com>
2018-04-07 00:23:14 -0500

    include JavaScritp  con nombre del controlador

---
+++ ControlB/Models/BreadcrumModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlB.Models
+{
+    public class BreadcrumModel
+    {
+        public string PaginaActual { get; set; }
+        public string PaginaPrincipal { get; set; }
+        public string UrlPaginaPrincipal { get; set; }
+    }
+}
